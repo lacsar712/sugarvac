e3e8e5204f41efdc6a6a4b6d43cca7366a7b6080
lacsar712 <lacsar712@users.noreply.github.com>
2026-08-27 09:16:12 +0800

    init: sugarvac

---
+++ internal/model/snapshot.go
@@ -0,0 +1,56 @@
+package model
+
+import "time"
+
+func CloneSnapshot(s PlantSnapshot) PlantSnapshot {
+	out := s
+	out.Alarms = append([]AlarmEvent(nil), s.Alarms...)
+	return out
+}
+
+func DefaultSnapshot(unitID string) PlantSnapshot {
+	now := time.Now()
+	return PlantSnapshot{
+		UnitID: unitID,
+		State:  StateColdStandby,
+		Settings: PlantSettings{
+			Mode:              ModeBaseLoad,
+			TargetMW:          150,
+			TargetSteamPSI:    NormalSteamPressurePSI,
+			MassecLevelSetpoint: 55,
+			FeedwaterFlowTPH:  400,
+			SteamFlowTPH:       35,
+			ExcessO2Setpoint:  3.5,
+		},
+		Plant: PlantRef{UnitLabel: unitID, PlantCode: "STEAM-PLT"},
+		Massec: MassecReading{
+			LevelPercent: 50,
+			Condition:    MassecNormal,
+			FeedwaterTPH: 0,
+			SteamFlowTPH: 0,
+		},
+		Steamjet: SteamjetReading{
+			BurnerPhase: BurnerIdle,
+		},
+		Vacpan: VacpanReading{
+			SteamPressurePSI: 0,
+			SteamTempF:       70,
+		},
+		UpdatedAt: now,
+	}
+}
+
+func (s PlantSnapshot) IsFiring() bool {
+	return s.State == StateFiring || s.State == StateLoadFollow || s.State == StateRamp
+}
+
+func (s PlantSnapshot) MassecWithinLimits() bool {
+	return s.Massec.LevelPercent >= MinMassecLevelPercent && s.Massec.LevelPercent <= MaxMassecLevelPercent
+}
+
+func (s PlantSnapshot) PressureWithinLimits() bool {
+	if !s.IsFiring() {
+		return true
+	}
+	return s.Vacpan.SteamPressurePSI <= MaxSteamPressurePSI
+}
